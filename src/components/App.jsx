d2daaa293ede280cda1d824e16f5ab826555f428
Ihor Diuzhev <110743288+IhorDiu@users.noreply.github.com>
2023-04-10 02:22:24 +0300

    add_the_main_logic_todo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,65 @@
-export const App = () => {
+import './App.css';
+import { useState } from 'react';
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState('');
+
+  const addTodo = () => {
+    if (text.trim().length) {
+      setTodos([
+        ...todos,
+        {
+          id: new Date().toISOString(),
+          text,
+          completed: false,
+        },
+      ]);
+    }
+    setText('');
+  };
+
+  const toggleTodoComplete = todoId => {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id !== todoId) return todo;
+
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      })
+    );
+  };
+
+  const removeTodo = todoId => {
+    setTodos(todos.filter(todo => todo.id !== todoId));
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="App">
+      <label>
+        <input value={text} onChange={e => setText(e.target.value)} />
+        <button onClick={addTodo}>Add Todo</button>
+      </label>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleTodoComplete(todo.id)}
+            />
+            <span>{todo.text}</span>
+            <span className="delete" onClick={() => removeTodo(todo.id)}>
+              &times;
+            </span>
+            {/* style={{ color: 'red' }} */}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
+
+export default App;
